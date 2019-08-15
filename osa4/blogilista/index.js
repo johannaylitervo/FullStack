be297f7fbac15b93c1d1dcf957a1ae7c0710152f
Johanna Ylitervo <johanna.ylitervo@gmail.com>
2019-08-15 16:06:10 +0300

    Lisäominaisuuksia koodiin, eslint + dotenv

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,21 +1,20 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const blogSchema = mongoose.Schema({
   title: String,
-  author: String,
+    author: String,
   url: String,
   likes: Number
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-//TÄSTÄ SALASANA POIS
-//const mongoUrl = 'mongodb+srv://johannay:@cluster0-byuyz.mongodb.net/blog-app?retryWrites=true&w=majority'
+const mongoUrl = process.env.MONGODB_URI
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
@@ -40,7 +39,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- osa4/blogilista/mongo.js
+++ osa4/blogilista/mongo.js
@@ -19,6 +19,16 @@ const blogSchema = mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+}
+
+)
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 const blog = new Blog({
