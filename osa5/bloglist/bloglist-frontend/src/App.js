e89b9891436e1ed370c52a66650ca6bb9ab2c86d
Johanna Ylitervo <johanna.ylitervo@gmail.com>
2019-09-16 17:15:37 +0300

    5:6 lisätty ominaisuus blogin tietojen laajennukseen

--- osa5/bloglist/bloglist-frontend/src/App.js
+++ osa5/bloglist/bloglist-frontend/src/App.js
@@ -164,6 +164,22 @@ const App = () => {
     )
   }
 
+  const showBlogs = () => {
+    return (
+      <div>
+      {
+          blogs.map(blog => 
+            <Blog 
+              key={blog.id} 
+              blog={blog} 
+            
+            />
+            )
+      }
+      </div>
+    )
+  }
+
   return (
     <div>
      <h1>Blogs</h1>
@@ -178,10 +194,8 @@ const App = () => {
       </div>
       }
       {
-          blogs.map(blog => 
-            <Blog key={blog.id} blog={blog}/>
-            )
-        }
+          showBlogs()
+      }
      </div>
   )
 }
--- osa5/bloglist/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ 
+  blog, 
+  blogState
+ }) => {
+  
+  const [ blogExtended, setBlogState ] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if(!blogExtended) {
+    return (
+      <div className={"blogname"} onClick={() => setBlogState(!blogExtended)} >
+      {blog.title} by {blog.author}
+      </div>
+    ) 
+  }
+  else {
+    return (
+      <div style={blogStyle}>
+        <div className={"blogname"} onClick={() => setBlogState(!blogExtended)}>
+          {blog.title} by {blog.author}
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
